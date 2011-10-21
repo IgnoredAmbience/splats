cc70004f09aa0da069e0a7d54bd6b07a0ae269bd
Thomas Wood <grand.edgemaster@gmail.com>
2011-10-21 13:52:56 +0100

    Initial scratchings in the dirt

---
+++ samples/LinkedList.rb
@@ -0,0 +1,48 @@
+class LinkedList
+  def initialize(item = nil)
+    @item = nil
+    @next = nil
+    push item if item
+  end
+
+  # Recursive
+  def push(item)
+    if @item.nil?
+      @item = item
+    elsif @next.nil?
+      @next = LinkedList.new item
+    else
+      @next.push item
+    end
+
+    self
+  end
+
+  def pop
+    if @next.nil?
+      i = @item
+      @item = nil
+      unless i.nil?
+        return i
+      else
+        raise "List is empty"
+      end
+    end
+
+    elem = self
+
+    while elem
+      if elem.next.next.nil?
+        i = elem.next.item
+        elem.next = nil
+        return i
+      else
+        elem = elem.next
+      end
+    end
+  end
+
+  protected 
+  attr_accessor :item, :next
+end
+
