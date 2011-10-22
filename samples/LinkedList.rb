63aa60806d181592e6c9cd57b135721a738ee07b
Thomas Wood <grand.edgemaster@gmail.com>
2011-10-22 17:43:37 +0100

    LinkedList: Delete

--- samples/LinkedList.rb
+++ samples/LinkedList.rb
@@ -23,6 +23,31 @@ class LinkedList
     @item == item or (not @next.nil? and @next.include? item)
   end
 
+  def delete!(item)
+    prev = nil
+    curr = self
+
+    while curr
+      if curr.item == item
+        if prev
+          prev.next = curr.next
+          return self
+        else
+          if curr.next
+            return curr.next
+          else
+            curr.item = nil
+            return curr
+          end
+        end
+      else
+        prev = curr
+        curr = curr.next
+      end
+    end
+  end
+
+
   # Iterative
   def pop
     if @next.nil?
