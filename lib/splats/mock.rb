3b1f4ded893b8849275933ce0e0c03f7863e7df6
Thomas Wood <grand.edgemaster@gmail.com>
2011-10-25 15:27:49 +0100

    Mock object

---
+++ lib/splats/mock.rb
@@ -0,0 +1,11 @@
+# Mock object to be passed into methods as an unknown parameter
+# Inherit from BasicObject so we can have as empty a class as possibly possible
+#
+# Note: This means that the Kernel module is not included, so you need to
+# explicity state Kernel for any builtin functions!
+
+class Mock < BasicObject
+  def method_missing(symbol, *args, &block)
+    ::Kernel.puts "Method '#{symbol}' called with arguments #{args} and #{block.nil? && 'no' || 'a'} block"
+  end
+end
