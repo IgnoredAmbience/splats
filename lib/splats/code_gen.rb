dd4cb24318f85896608850053d5a53461a0d4e7e
Ethel Bardsley <ethel.bardsley@gmail.com>
2011-10-31 17:35:20 +0000

    Initial basic generator
    Only works with things with a to_s, naieve as hell

---
+++ lib/splats/code_gen.rb
@@ -0,0 +1,23 @@
+def argsToString (argsList)
+  return nil if argsList.nil?
+  out = ""
+  out << "("
+  argsList[0..-2].each{ |a| out << "#{a}," }
+  out << "#{argsList[-1]}"
+  out << ")"
+end
+
+def printLine (line)
+  if (line.out.nil?)
+    puts "#{line.obj}.#{line.meth}#{argsToString line.args}"
+  else
+    puts "#{line.out} = #{line.obj}.#{line.meth}#{argsToString line.args}"
+  end
+end
+
+def print (codeList)
+  puts "def #{codeList.hash}"
+  codeList[0..-2].each{ |line| printLine line }
+  puts "assert_equal #{codeList[-1].obj}.#{codeList[-1].meth}#{argsToString line.args}, #{codeList[-1].out}"
+  puts "end"
+end
