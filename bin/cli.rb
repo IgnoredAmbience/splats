1fc9bce0df14b0dff8eb232759e92309dc9c2586
Joseph Slade <js5409@doc.ic.ac.uk>
2011-10-31 17:44:56 +0000

    Added loading from files, tidied cli too

--- bin/cli.rb
+++ bin/cli.rb
@@ -1,5 +1,6 @@
 require 'optparse'
-require_relative '../lib/splats/core.rb'
+require_relative '../lib/splats.rb'
+
 
 #options parsed so far
 options = {}
@@ -10,11 +11,10 @@ options = {}
 
 OptionParser.new do |opts|
 
-#  This is the general shape of a flag option
-#  opts.on("--some-flag", "-s", "Flag description") do |flag|
-#    options[:flagName] = flag
-#    puts flag
-#  end
+  #  This is the general shape of a flag option
+  #  opts.on("--some-flag", "-s", "Flag description") do |flag|
+  #    options[:flagName] = flag
+  #  end
 
   opts.on("--help", "-h", "Display help screen") do |flag|
     puts opts
@@ -25,17 +25,23 @@ OptionParser.new do |opts|
   # optparser that this flag expects an argument
   opts.on("--object-from-file FILE", "-f", "Test class from file. File & class must share names") do |flag|
 
-    if (not (require flag)) #try to load the passed in file
+    if (not (require_relative flag)) #try to load the passed in file
       puts "Failed to load #{flag}"
       exit
     end
 
-#    In here we need some logic to load and then test the class
+    # strip out absolute file name (i.e. the class name) & pass this class off to the tester
+    # Kernel.const_get gives the class with the name as the flag
+    flagAsSymbol = File.basename(flag,'.rb').to_s # .rb parameter lops .rb off the end
+    flagAsClass = Kernel.const_get(flagAsSymbol)
+
+    t = Splats::Core.new(flagAsClass)
+    t.test_class
 
     exit
   end
 
 
 
-# end.parse! is doable because OptionParse.new return itself if passed a block
+  # end.parse! is doable because OptionParse.new return itself if passed a block
 end.parse!
