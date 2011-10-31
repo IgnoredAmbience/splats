bc3073b5e94a1c889ad17347dccc108193f61dd3
Thomas Wood <grand.edgemaster@gmail.com>
2011-10-31 12:36:00 +0000

    Merge chris' code

--- lib/splats/core.rb
+++ lib/splats/core.rb
@@ -2,7 +2,6 @@ module SPLATS
   class Core
     def initialize(c)
       @class = c
-      puts c.new
     end
 
     def test_class
@@ -16,34 +15,36 @@ module SPLATS
         test_method @obj.method(m)
       end
     end
+
     def test_method(method, params=nil)
-        params = get_params(method) if params.nil?
-        req_params, opt_params, rest_params, block_params  = params
-        puts "%s has %d required parameters, %d optional parameters, %s extra parameters and %s block." %
-            [method, (req_params ? req_params.length : 0), (opt_params ? opt_params.length : 0),
-                    (rest_params ? "some" : "no"), (block_params ? "a" : "no")]
-          puts "\n"
+      params = get_params(method) if params.nil?
+      req_params, opt_params, rest_params, block_params = params
 
-          args = Array.new(req_params) { Mock.new }
-          [args, method.call(*args)]
+      puts "%s has %d required parameters, %d optional parameters, %s extra parameters and %s block." %
+      [method, req_params.length, opt_params.length,
+        (rest_params ? "some" : "no"), (block_params ? "maybe a" : "no")]
+
+      args = Array.new(req_params.length) { Mock.new }
+      [args, method.call(*args)]
     end
-    def get_params(method)
-        params = method.parameters
-        req_params, opt_params,  = [], []
-        for p in params
-             case p[0]
-                when :req
-                    req_params.push p[1]
-                when :opt
-                    opt_params.push p[1]
-                when :rest
-                    rest_params = p[1]
-                when :block
-                    block_params = p[1]
-            end
-        end
-        [req_params, opt_params, rest_params, block_params]
-    end   
 
+    def get_params(method)
+      params = method.parameters
+      req_params, opt_params = [], []
+
+      for p in params
+        case p[0]
+        when :req
+          req_params.push p[1]
+        when :opt
+          opt_params.push p[1]
+        when :rest
+          rest_params = p[1]
+        when :block
+          block_params = p[1]
+        end
+      end
+      [req_params, opt_params, rest_params, block_params]
+    end   
   end
 end
