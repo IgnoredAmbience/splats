21abf6fcc1868b7975fe7aafe7f1700f50c200e3
Thomas Wood <tw1509@doc.ic.ac.uk>
2011-10-31 16:43:26 +0000

    Tidying

--- lib/splats.rb
+++ lib/splats.rb
@@ -3,4 +3,17 @@ require_relative 'splats/core'
 require_relative 'splats/mock'
 
 module SPLATS
+  def load_config(filename)
+    require "yaml"
+    config = YAML.load_file(filename)
+
+    # Load the first version modules
+    config["modules"].each{ |module_name, classes|
+      load root_dir + "/" + config["versions"][0] + "/" + module_name
+      classes.each{ |c|
+        s = Core.new(Object.const_get(c))
+        s.test_class
+      }
+    }
+  end
 end
