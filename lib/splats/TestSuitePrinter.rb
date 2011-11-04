547cdf69b91983ca7422386d249538a1cfcad473
Ethel Bardsley <ethel.bardsley@gmail.com>
2011-11-04 15:02:19 -0400

    yet more documentation

--- lib/splats/TestSuitePrinter.rb
+++ lib/splats/TestSuitePrinter.rb
@@ -10,24 +10,29 @@ class TestSuitePrinter
     @tests = tests
   end
 
+  # Returns the final ruby code
   def print
     (requirements + header + tests + footer).join("\n")
   end
 
 private
 
+  # The list of require statements
   def requirements
     @reqs.map{ |r| "require '#{r}'" }
   end
 
+  # The class header
   def header
     ["class #{@name} < Test::Unit::TestCase"]
   end
 
+  # The list of test methods
   def tests
     @tests.map{|t| t.print}
   end
   
+  # The class footer
   def footer
     ["end"]
   end
