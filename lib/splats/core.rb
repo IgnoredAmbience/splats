9906e0a07cd8fb3e908b4884240675be3281c1c0
Chris K <pickledjesus@gmail.com>
2011-11-04 21:09:55 +0000

    Comment stuff for RDoc

--- lib/splats/core.rb
+++ lib/splats/core.rb
@@ -14,19 +14,19 @@ module SPLATS
     def to_s
       ""
     end
-
-    def initialize_tree
-      # We need to instantiate first, this may result in multiple returned
-      # objects, if we can instantiate with parameters
-      # We can only read the number of parameters from the initialize method,
-      # since, def Class.new(*args), prevents any useful detail from being
-      # exposed
+    
+    # We need to instantiate first, this may result in multiple returned
+    # objects, if we can instantiate with parameters. <br>
+    # We can only read the number of parameters from the initialize method,
+    # as def Class.new(*args), prevents any useful detail from being exposed
+    def initialize_tree      
       m = @class.method :new
       im = @class.instance_method :initialize
       tree = Tree::TreeNode.new "CONSTRUCTOR", m
       generate_parameters! tree, im
     end
 
+    # Goes through the tree doing magic
     def test_class(depth = 5)
       depth.times do
         @tree.each_leaf do |leaf|
@@ -38,7 +38,8 @@ module SPLATS
         expand_tree
       end
     end
-
+    
+    #Adds a new node to the leaf based on Tom magic
     def expand_tree
       @tree.each_leaf do |leaf|
         if leaf.content
@@ -50,7 +51,9 @@ module SPLATS
         end
       end
     end
-
+    
+    #Creates a list of Mock objects based on the number of optional parameters
+    #For example, req=2, opt=1 gives [[M, M], [M, M, M]]
     def generate_parameters!(node, method=nil)
       method ||= @class.instance_method node.content
 
--- lib/splats/mock.rb
+++ lib/splats/mock.rb
@@ -1,11 +1,11 @@
-# Mock object to be passed into methods as an unknown parameter
-# Inherit from BasicObject so we can have as empty a class as possibly possible
-#
-# Note: This means that the Kernel module is not included, so you need to
-# explicity state Kernel for any builtin functions!
-
+# SPLATS - SpLATS Lazy Automated Testing System
 module SPLATS
+  # Mock object to be passed into methods as an unknown parameter
+  # Inherit from BasicObject so we can have as empty a class as possible <br>
+  # Note: This means that the Kernel module is not included, so you need to
+  # explicity state Kernel for any builtin functions!
   class Mock < BasicObject
+    # Prints information about the failed method call
     def method_missing(symbol, *args, &block)
       ::Kernel.puts "Method '#{symbol}' called with arguments #{args} and #{block.nil? && 'no' || 'a'} block"
       if symbol == :inspect
