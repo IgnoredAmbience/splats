e12c0e4aaf436be48d23ff0611868a8d12c1723c
Thomas Wood <grand.edgemaster@gmail.com>
2011-11-09 01:16:30 +0000

    Hook defaulted BasicObject methods

--- lib/splats/mock.rb
+++ lib/splats/mock.rb
@@ -5,6 +5,14 @@ module SPLATS
   # Note: This means that the Kernel module is not included, so you need to
   # explicity state Kernel for any builtin functions!
   class Mock < BasicObject
+    # Rebind all defaulted methods to run via method_missing
+    (instance_methods - instance_methods(include_super = false) -
+                                                   [:__send__]).each do |method|
+      define_method(method) do |*args, &block|
+        __send__(:method_missing, method, *args, &block)
+      end
+    end
+
     # Prints information about the failed method call
     def method_missing(symbol, *args, &block)
       ::Kernel.puts "Method '#{symbol}' called with arguments #{args} and #{block.nil? && 'no' || 'a'} block"
