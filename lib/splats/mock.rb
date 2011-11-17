5692a3a98971c66fe47718267530dcccf9497fde
Thomas Wood <grand.edgemaster@gmail.com>
2011-11-17 17:43:38 +0000

    Mock messing

--- lib/splats/mock.rb
+++ lib/splats/mock.rb
@@ -5,24 +5,52 @@ module SPLATS
   # Note: This means that the Kernel module is not included, so you need to
   # explicity state Kernel for any builtin functions!
   class Mock < BasicObject
-    # Rebind all defaulted methods to run via method_missing
-    (instance_methods - instance_methods(false) -
-                                                   [:__send__]).each do |method|
-      define_method(method) do |*args, &block|
-        __send__(:method_missing, method, *args, &block)
-      end
+    # Rebind all inherited public methods to run via method_missing
+    (instance_methods - instance_methods(false)).each do |method|
+      private method
     end
 
     # Prints information about the failed method call
     def method_missing(symbol, *args, &block)
       ::Kernel.puts "Method '#{symbol}' called with arguments #{args} and #{block.nil? && 'no' || 'a'} block"
-      if symbol == :inspect
-        "<Mock Object>"
+      if Mock.private_instance_methods.include?(symbol)
+        __send__(symbol, *args, &block)
+      else
+        ::Kernel.puts "  No binding found"
+        nil
       end
     end
 
     def __SPLATS_is_mock?
       true
+    end
+
+    private
+    # Note the defaulted inclusion of [:==, :equal?, :!, :!=, :instance_eval,
+    # :instance_exec, :__send__, :__id__, :initialize, :singleton_method_added,
+    # :singleton_method_removed, :singleton_method_undefined]
+
+    # We cannot evaluate an object to false, it just /isn't/ possible in ruby.
+    # But we /can/ (re)define methods on nil!
+
+    def inspect
+      "<Mock Object>"
+    end
+
+    def to_r
+      0
+    end
+
+    def coerce
+      [args[0], to_r]
+    end
+
+    def to_str
+      ""
+    end
+
+    def to_ary
+      []
     end
   end
 end
