d6812eca9d756879a12db7901c7241e618a9730d
Ethel Bardsley <ethel.bardsley@gmail.com>
2011-11-19 22:29:26 +0000

    Started complete rewrite of cli

--- bin/cli.rb
+++ bin/cli.rb
@@ -1,47 +1,54 @@
 require 'optparse'
 require_relative '../lib/splats.rb'
 
-
 #options parsed so far
 options = {}
 
-# This optparse library appears to be pretty damn cool
 # Using puts on the opts object (the parameter in the new block for OptionParser)
 #   prints out the long & short versions of a flag as well as a description of its purpose
 
-OptionParser.new do |opts|
+optparse = OptionParser.new do |opts|
 
-  #  This is the general shape of a flag option
-  #  opts.on("--some-flag", "-s", "Flag description") do |flag|
-  #    options[:flagName] = flag
-  #  end
+  opts.banner = "Usage: cli.rb [options]"
 
-  opts.on("--help", "-h", "Display help screen") do |flag|
+  opts.on("--help", "-h", "Display help screen") do
     puts opts
     exit
   end
 
-  # Apparently I need to put FILE in CAPS to tell
-  # optparser that this flag expects an argument
-  opts.on("--object-from-file FILE", "-f", "Test class from file. File & class must share names") do |flag|
-
-    if (not (require_relative flag)) #try to load the passed in file
-      puts "Failed to load #{flag}"
-      exit
-    end
-
-    # strip out absolute file name (i.e. the class name) & pass this class off to the tester
-    # Kernel.const_get gives the class with the name as the flag
-    flagAsSymbol = File.basename(flag,'.rb').to_s # .rb parameter lops .rb off the end
-    flagAsClass = Kernel.const_get(flagAsSymbol)
-
-    t = Splats::Core.new(flagAsClass)
-    t.test_class
-
-    exit
+  options[:list] = nil
+  opts.on("--object-from-file FILES", "-f", Array, "Test classes from files. File & class must share names") do |files|
+    options[:list] = files
   end
 
+  options[:config] = nil
+  opts.on("--config FILE", "-c", "Configuration file") do |file|
+    options[:config] = file
+  end
+
+  options[:outdir] = nil
+  opts.on("--output-directory DIR", "-o", "Output directory (defaults to \"tests\")") do |dir|
+    options[:outdir] = dir
+  end
+
+end
 
 
-  # end.parse! is doable because OptionParse.new return itself if passed a block
-end.parse!
+begin
+  optparse.parse!
+
+  # TODO: Other things may want to go here, eg, if config, assume outdir specified there?
+  if !(options[:list].nil? ^ options[:config].nil?)
+    puts optparse
+    exit
+  end
+rescue OptionParser::InvalidOption,OptionParser::MissingArgument
+  puts optparse
+  exit
+end
+
+if (!options[:list].nil?)
+  SPLATS.load_classes(options[:config])
+elsif (!options[:config].nil?)
+  SPLATS.load_config(options[:config])
+end
