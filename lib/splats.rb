16f4ceaaa400cf1bbb12ae267d74662dc5f2c237
Ethel Bardsley <ethel.bardsley@gmail.com>
2011-11-20 17:31:36 +0000

    Rearranged things
    Cleaned up naming, dead code and unnecessarily public methods

--- lib/splats.rb
+++ lib/splats.rb
@@ -17,7 +17,7 @@ module SPLATS
   #
   # @param [String] filename The ruby source file to load
   # @return [Array<Class>] The classes defined within the file
-  def self.load_class filename
+  def self.load_classes filename
     constants = Module.constants
 
     load filename
@@ -36,12 +36,22 @@ module SPLATS
     #
     # @note Directory created if necessary
     def initialize(input_file, output_dir)
-      @input_classes = SPLATS.load_class input_file
+      @input_classes = SPLATS.load_classes input_file
       @output_dir = output_dir
       if not File::directory?(output_dir)
         Dir.mkdir(output_dir)
       end
     end
+    
+    # Creates tests for every class in the given file
+    def test_classes
+      @input_classes.each do |c|
+        single_class_test(c)
+      end
+    end
+
+
+		private
     
     # Creates tests for a class by generating and traversing the tree
     # then generating the code from the abstract syntax
@@ -49,18 +59,9 @@ module SPLATS
     # @param [Class] testing_class The class to be tested
     def single_class_test(testing_class)
       cur_testing_class = Generator.new(testing_class)
-      test_counter = 0
       cur_testing_class.test_class do |test, result|
         test_printer = SPLATS::TestPrinter.new(test, result)
-        write(test_printer.print, testing_class ,test_counter)
-        test_counter += 1
-      end
-    end
-    
-    # Creates tests for every class in @input_classes 
-    def multi_class_test 
-      @input_classes.each do |cla|
-        single_class_test(cla)
+        write(test_printer.print, testing_class)
       end
     end
 
@@ -68,10 +69,9 @@ module SPLATS
     # Writes to a file - ouput_directory/test_className01.rb     
     #
     # @param [String] test_as_string The finished generated test
-    # @param [Class] cla The class being tested
-    # @param [Numeric] id Test counter
-    def write(test_as_string, cla, id)
-      File.open("#{@output_dir}test_#{cla}#{id}.rb", "w") do |x|
+    # @param [Class] klass The class being tested
+    def write(test_as_string, klass)
+      File.open("#{@output_dir}test_#{klass}.rb", "w") do |x|
         x.puts test_as_string
       end
     end
