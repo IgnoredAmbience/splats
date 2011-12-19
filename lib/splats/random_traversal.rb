296e05dc166cbd97f48fd4279e769ecb05fd72f2
Thomas Wood <grand.edgemaster@gmail.com>
2011-12-19 16:47:31 +0000

    Rejig RandomTraversal to use the Random class
    Random gives us the seed state we need, it will also prevent conflicts between
    SpLATS and the program under test, if it uses the system-wide rand functions.
    Also add continue_generation? with a probability of 1/10 stopping.

--- lib/splats/random_traversal.rb
+++ lib/splats/random_traversal.rb
@@ -4,31 +4,33 @@ module SPLATS
     include Traversal
     # If there's already a seed in use, then initialise the RNG with the seed
     # Also keeps an eye on the seed, for possibly returning it in the future
-    def initialize seed=nil
-      # I think I'm failing to understand properly how this works, but it seems the only way to get guaranteed results is to run the srand twice and store that number
-      if seed
-        srand(seed)
-        @rand = srand(seed)
-      else
-        @rand = srand()
-      end
+    def initialize seed=0
+      @rng = Random.new seed
     end
 
-    # This is passed a list of methods - will return a randomly selected index
+    def seed
+      @rng.seed
+    end
+
+    # This is passed a list of methods - will return a randomly selected item
     def select_method methods
-      methods[rand(methods.length)]
+      methods[@rng.rand(methods.length)]
     end
 
     def select_arguments arguments
-      arguments[rand(arguments.length)]
+      arguments[@rng.rand(arguments.length)]
     end
 
     def select_decision decisions
-      decisions[rand(decisions.length)]
+      decisions[@rng.rand(decisions.length)]
     end
 
     def continue_descent?
-      rand(2) == 0 ? false : true
+      @rng.rand(2) == 0
+    end
+
+    def continue_generation?
+      @rng.rand(10) != 0
     end
   end
 end
