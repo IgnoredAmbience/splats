89fb36a09c3d19ee75755d715ee4b3434189e3fd
Thomas Wood <grand.edgemaster@gmail.com>
2011-12-24 02:30:47 +0000

    Some tests

---
+++ test/tc_traversal.rb
@@ -0,0 +1,50 @@
+require_relative '../lib/splats/traversal'
+require 'test/unit'
+
+class Traversal
+  include SPLATS::Traversal
+end
+
+class TraversalTest < Test::Unit::TestCase
+  def test_select_method
+    assert_raises NotImplementedError do
+      Traversal.new.select_method []
+    end
+  end
+
+  def test_select_arguments
+    assert_raises NotImplementedError do
+      Traversal.new.select_arguments []
+    end
+  end
+
+  def test_select_decision
+    assert_raises NotImplementedError do
+      Traversal.new.select_decision []
+    end
+  end
+
+  def test_continue_descent?
+    assert_raises NotImplementedError do
+      Traversal.new.continue_descent?
+    end
+  end
+
+  def test_continue_generation?
+    assert_raises NotImplementedError do
+      Traversal.new.continue_generation?
+    end
+  end
+
+  def test_notify_new_traversal
+    assert_nothing_raised do
+      Traversal.new.notify_new_traversal
+    end
+  end
+
+  def test_notify_exception_raised
+    assert_nothing_raised do
+      Traversal.new.notify_exception_raised
+    end
+  end
+end
--- test/test_splats.rb
+++ test/test_splats.rb
@@ -2,8 +2,8 @@ require 'simple_cov'
 SimpleCov.start
 
 $:.unshift File.join(File.dirname(__FILE__),'..','lib')
+$:.unshift File.join(File.dirname(__FILE__))
 
-require 'splats.rb'
-
-
+require 'splats'
+require 'tc_traversal'
 
